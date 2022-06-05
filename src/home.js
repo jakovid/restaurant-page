35f37c6fef5bf195b2ca62ad4689894c54bd5a19
jakovid <jacobk.dowdle@gmail.com>
2022-06-05 17:29:57 +0800

    added functions to build header and body for the home page module

--- src/home.js
+++ src/home.js
@@ -1,42 +1,60 @@
-// function loadHomePage() {
-// let content = document.getElementById('content');
-// let div = document.createElement('div');
-// div.innerText = "Hello World"
-// content.appendChild(div);
-
-// }
-
 var loadHomePage = {
     content: document.getElementById('content'),
     buildHomePage: function(){
-        this.buildTabBrowse();
+        this.buildHeader();
+        this.buildBody();
     },
-    buildTabBrowse: function() {
+    buildHeader: function() {
         //create elements
+        let headerContainer = document.createElement('div');
+        let headerTitle = document.createElement('div');
         let tabContainer = document.createElement('div');
         let homeTab = document.createElement('button');
         let menuTab = document.createElement('button');
         let contactTab = document.createElement('button');
         //add class names to elements
+        headerContainer.className = "headerContainer";
+        headerTitle.className = "headerTitle";
         tabContainer.className = "tabContainer";
         homeTab.className = "homeTab";
         menuTab.className = "menuTab";
         contactTab.className = "contactTab";
         //add inner text to buttons
+        headerTitle.innerText = "Luna's Italian"
         homeTab.innerText = "Home";
         menuTab.innerText = "Menu";
         contactTab.innerText = "Contact";
         //append elements
         tabContainer.append(homeTab,menuTab,contactTab);
-        this.content.appendChild(tabContainer);
+        headerContainer.append(headerTitle,tabContainer);
+        this.content.appendChild(headerContainer);
     },
-    test: function() {
-        let div = document.createElement('div');
-        div.innerText = "Hello World"
-        content.appendChild(div);
+    buildBody: function(){
+        //create elements
+        let bodyContainer = document.createElement('div');
+        let bodyTitle = document.createElement('div');
+        let bodyTagline = document.createElement('div');
+        let bodyImage = document.createElement('img');
+        let bodyCallToAction = document.createElement('div');
+        //add class names to elements
+        bodyContainer.className = "bodyContainer";
+        bodyTitle.className = "bodyTitle";
+        bodyTagline.className = "bodyTagline";
+        bodyImage.classname = "bodyImage";
+        bodyCallToAction.classname = "bodyCTA";
+        //add innertext to Elements
+        bodyTitle.innerText = "Taiwan's Best Italian Restaurant!";
+        bodyTagline.innerText = "Open to the world since 2022!";
+        bodyCallToAction.innerText = "Order online or visit our nearest location!";
+        //append elements
+        bodyContainer.append(bodyTitle,bodyTagline,bodyImage,bodyCallToAction);
+        this.content.appendChild(bodyContainer);
+        console.log('body test!');
     },
+    buildFooter: function(){
 
-}
+    }
 
+};
 
 export { loadHomePage };
