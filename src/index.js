2326b1e79641d8ed9478617dbdcf2d9adf5b2b39
jakovid <jacobk.dowdle@gmail.com>
2022-06-06 23:43:39 +0800

    deleted unneeded line

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import { loadHomePage } from "./home";
 import { loadMenuPage } from "./menu";
 import { header } from "./header";
 
-// loadHomePage.buildHomePage();
-
 header.buildHeader();
 loadMenuPage.buildMenuPage();
