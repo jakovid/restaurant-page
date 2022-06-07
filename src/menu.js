38a07213715bb0cad0158d1f9b101127a497f1a8
jakovid <jacobk.dowdle@gmail.com>
2022-06-07 21:01:41 +0800

    added images and menu information

--- src/menu.js
+++ src/menu.js
@@ -15,6 +15,7 @@ var loadMenuPage = {
         let saladName = document.createElement('div');
         let saladAbout = document.createElement('div');
         //add src for pictues
+        friesPic.src = '../img/fries.png';
 
         //add class names to elements
         menuContainer.className = 'menuContainer';
@@ -24,10 +25,17 @@ var loadMenuPage = {
         friesName.className = "foodName";
         friesAbout.className = "foodAbout";
         //add innertext to elements
-        
+        friesName.innerText = "Garlic Fries";
+        friesAbout.innerText = "Fresh cut spuds"
+        saladName.innerText = "Green Goddess Salad";
+        saladAbout.innerText = "You've seen this one too many times on tiki toki";
+
         //append elements
         friesBox.append(friesPic,friesName,friesAbout);
         saladBox.append(saladPic,saladName,saladAbout);
+        menuContainer.append(friesBox,saladBox);
+        this.content.appendChild(menuContainer);
+        console.log('Menu Test!');
         
 
     }
