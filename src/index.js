560a1521e78feb82da7fce0b68489126ad33c9bd
jakovid <jacobk.dowdle@gmail.com>
2022-06-07 20:46:22 +0800

    added a bunch of stuff to the dom

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { footer } from "./footer";
 (function() {
     var buildWebsite = {
         init: function() {
-            this.homePage();
+            this.homePageInit();
             this.cacheDom();
             this.bindEvents();
             console.log('this is a big test');
@@ -15,23 +15,33 @@ import { footer } from "./footer";
             this.el = document.getElementById('content');
             this.homeTab = this.el.querySelector('.homeTab');
             this.menuTab = this.el.querySelector('.menuTab');
-            this.contactTab = this.el.querySelector('.contactTab')
+            this.contactTab = this.el.querySelector('.contactTab');
         },
         bindEvents: function(){
-            this.homeTab.onclick = this.homePage();
-            this.menuTab.onclick = this.menuPage();
+            this.homeTab.onclick = this.homePage;
+            this.menuTab.onclick = this.menuPage;
             // this.contactTab.onclick = 
         },
-        homePage: function() {
+        homePageInit: function() {
             header.buildHeader();
             loadHomePage.buildHomePage();
             footer.buildFooter();
         },
+        homePage: function() {
+            buildWebsite.el.innerHTML = '';
+            header.buildHeader();
+            loadHomePage.buildHomePage();
+            footer.buildFooter();
+            buildWebsite.cacheDom();
+            buildWebsite.bindEvents();
+        },
         menuPage: function() {
+            buildWebsite.el.innerHTML = '';
             header.buildHeader();
             loadMenuPage.buildMenuPage();
             footer.buildFooter();
-            console.log('click!');
+            buildWebsite.cacheDom();
+            buildWebsite.bindEvents();
         }
 
 
@@ -39,19 +49,3 @@ import { footer } from "./footer";
 
     buildWebsite.init();
 })()
-
-// import { loadHomePage } from "./home";
-// import { loadMenuPage } from "./menu";
-// import { header } from "./header";
-// import { footer } from "./footer";
-
-// var buttons = {
-//     home: document.getElementsByClassName('homeTab'),
-//     menu: document.getElementsByClassName('menuTab'),
-    
-
-// }
-
-// header.buildHeader();
-// loadHomePage.buildHomePage();
-// footer.buildFooter();
--- src/menu.js
+++ src/menu.js
@@ -1,7 +1,35 @@
 var loadMenuPage = {
     content: document.getElementById('content'),
     buildMenuPage: function(){
+        //create elements
+        let menuContainer = document.createElement('div');
+        let menuTitle = document.createElement('div');
+        //create fries
+        let friesBox = document.createElement('div');
+        let friesPic = document.createElement('img');
+        let friesName = document.createElement('div');
+        let friesAbout = document.createElement('div');
+        //create salad
+        let saladBox = document.createElement('div');
+        let saladPic = document.createElement('img');
+        let saladName = document.createElement('div');
+        let saladAbout = document.createElement('div');
+        //add src for pictues
+
+        //add class names to elements
+        menuContainer.className = 'menuContainer';
+        menuTitle.className = 'menuTitle';
+        friesBox.className = "foodBox";
+        friesPic.className = "foodPic";
+        friesName.className = "foodName";
+        friesAbout.className = "foodAbout";
+        //add innertext to elements
         
+        //append elements
+        friesBox.append(friesPic,friesName,friesAbout);
+        saladBox.append(saladPic,saladName,saladAbout);
+        
+
     }
 
 };
