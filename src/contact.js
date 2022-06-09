6db9b5e5ff562f4db356ea11767dc9448d728d0f
jakovid <jacobk.dowdle@gmail.com>
2022-06-09 15:05:21 +0800

    fixed footer display issues and started creating the contact.js page

---
+++ src/contact.js
@@ -0,0 +1,12 @@
+var loadContactPage = {
+    content: document.getElementById('content'),
+    buildContactPage: function() {
+        //create elements
+
+        //add class names
+
+        //add innertext to elements
+
+        //append elements
+    }
+}
